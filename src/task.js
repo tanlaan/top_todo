08bb6dccabb5e907468d4ea4f2ef9b5e780cbf9f
Chris Bolas <tanlaan33@gmail.com>
2020-10-27 20:51:34 -0700

    Added base files

---
+++ src/task.js
@@ -0,0 +1,11 @@
+// Todo Tasks 101:
+// title
+// priority
+// dueDate
+// task
+// note
+// tags
+
+const Task = (title, priority = 0, dueDate, task, note, tags) => {
+    return {title, priority, dueDate, task, note, tags}
+}
