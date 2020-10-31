b833ec8eb8d82e6e7f1141f5b59e89260cbabc86
Chris Bolas <tanlaan33@gmail.com>
2020-10-30 22:37:38 -0700

    renderTask implemented and tested

--- src/gui.js
+++ src/gui.js
@@ -1,6 +1,43 @@
-export function loadHeader() {
-    let main = document.getElementById('container')
-    let header = document.createElement('h1')
-    header.textContent = `Chris' Todo`
-    main.appendChild(header)
+export function loadHeader(element) {
+    let header = document.createElement('header')
+    let title = document.createElement('h1')
+    title.textContent = `Chris' Todo`
+    header.appendChild(title)
+    element.appendChild(header)
+}
+
+export function renderTask(element, task) {
+    let container = document.createElement('div')
+    container.setAttribute('class', 'task-box')
+
+    let title = document.createElement('h3')
+    title.textContent = task.title
+    container.appendChild(title)
+
+    let subtitle = document.createElement('p')
+    subtitle.textContent = `Priority: ${task.priority} | Due: ${task.dueDate} | Complete: ${task.complete}`
+    container.appendChild(subtitle)
+
+    let taskText = document.createElement('p')
+    taskText.textContent = task.task
+    container.appendChild(taskText)
+
+    let noteText = document.createElement('p')
+    noteText.textContent = task.noteText
+    container.appendChild(noteText)
+
+    let tagText = document.createElement('p')
+    tagText.textContent = task.tags
+    container.appendChild(tagText)
+
+    container.appendChild(document.createElement('hr'))
+
+    element.appendChild(container)
+    
+}
+
+function removeChildren(element) {
+    while(element.firstChild) {
+        element.removeChild(element.lastChild)
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import {Project, Task} from './task.js'
 
-import {loadHeader} from './gui.js'
+import {loadHeader, renderTask} from './gui.js'
 
 let defaultProject = new Project()
 
-loadHeader()
+let testTask = new Task('My test', 0, '01/01/21', 'This is a thing to do.', "Don't forget to do this.", ['tags', 'n', 'stuff'])
+let otherTask = new Task('Another one', 4, '06/01/21', 'We do the stuff.', "Don't forget to do this.", ['tags', 'n', 'stuff'])
+
+defaultProject.add(testTask)
+defaultProject.add(otherTask)
+
+let root = document.getElementsByTagName('MAIN')[0]
+
+loadHeader(root)
+
+for (let task in defaultProject.storage){
+    renderTask(root, defaultProject.storage[task])
+}
+
