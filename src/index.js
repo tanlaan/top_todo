bc2411cf9d198952585993d3af3e3e8c30011040
Chris Bolas <tanlaan33@gmail.com>
2021-05-05 22:34:59 -0700

    Firestore save functional, restore still broken

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import { renderPage } from './gui';
-import { expandState } from './state';
-
-var provider = new firebase.auth.GoogleAuthProvider()
-// Check if firebase has information for our user?
-// So that means we need to do user authentication now though...
-//
+import { getState } from './state';
 
 // Initialize projects storage
+//
 window.projects = [];
 
 // Check local storage for prior usage
-if (localStorage.getItem('projects')) {
-  window.projects = expandState(localStorage.getItem('projects'));
-} 
+// if (localStorage.getItem('projects')) {
+//   window.projects = expandState(localStorage.getItem('projects'));
+// } 
 
 const root = document.querySelector('main');
 renderPage(root, window.projects[0], window.projects);
